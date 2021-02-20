8c8f103b681ebb3b04b3ed41ba738812b2a50c05
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-02-21 03:30:58 +0600

    Add back to top button

--- src/js/index.js
+++ src/js/index.js
@@ -68,3 +68,22 @@ var swiper = new Swiper('.swiper-container.testimonial-slider', {
         prevEl: '.swiper-prev-button',
     },
 });
+
+// Back to top Button
+const backToTopBtn = document.getElementById('back-to-top');
+window.onscroll = function () {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.opacity = '1';
+        backToTopBtn.style.transform = 'scale(1)';
+    } else {
+        backToTopBtn.style.opacity = '0';
+        backToTopBtn.style.transform = 'scale(0)';
+    }
+}
+backToTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
