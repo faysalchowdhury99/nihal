7ae044000cbbcef5f7baf8fbdc448b1ad8811173
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-02-21 00:00:30 +0600

    Add Testimonial Section JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
-const swiper = new Swiper('.swiper-container.partner-slider', {
+//  Partner Slider
+var swiper = new Swiper('.swiper-container.partner-slider', {
     slidesPerView: 6,
     spaceBetween: 30,
+    grabCursor: true,
     breakpoints: {
         0: {
             slidesPerView: 2,
@@ -17,5 +19,51 @@ const swiper = new Swiper('.swiper-container.partner-slider', {
         1200: {
             slidesPerView: 6,
         },
+    },
+});
+
+// Testimonial Slider
+var swiper = new Swiper('.swiper-container.testimonial-slider', {
+    effect: 'coverflow',
+    initialSlide: 2,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 300,
+        depth: 400,
+        modifier: 1,
+        slideShadows: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            coverflowEffect: {
+                stretch: 200,
+            },
+        },
+        992: {
+            coverflowEffect: {
+                stretch: 780,
+            },
+        },
+        1200: {
+            coverflowEffect: {
+                stretch: 770,
+            },
+        },
+        1400: {
+            coverflowEffect: {
+                stretch: 900,
+            },
+        },
+    },
+    navigation: {
+        nextEl: '.swiper-next-button',
+        prevEl: '.swiper-prev-button',
     },
 });
